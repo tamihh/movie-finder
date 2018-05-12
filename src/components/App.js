a1b10de362c66d93f4899ad3be17f0cca7723fde
Tamiris Simoneli <tamiris.simoneli@gmail.com>
2018-05-12 19:18:12 -0300

    updat hot reload

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hot } from 'react-hot-loader'
+
 const App = () => {
   return (
     <div>
-      <p>React here!</p>
+      <p>React here! iuhduiai</p>
     </div>
   );
 };
-export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+
+export default hot(module)(App)
---
+++ webpack/client.dev.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+module.exports = {
+  name: 'client',
+  target: 'web',
+  devtool: 'inline-source-map',
+  devServer: {
+    host: 'localhost',
+    port: 8000,
+    historyApiFallback: true,
+    hot: true
+  },
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, '../src/index.js')
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      components: path.resolve('src/components'),
+    },
+    modules: [path.resolve('src/'), 'node_modules'],
+    extensions: ['.js', '.css', '.scss']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true
+    })
+  ]
+};
